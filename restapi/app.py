bfb0b9b023c8d09c13724a1f0e3544779847eae2
Pallab Panda <pallab.panda@gmail.com>
2024-07-23 18:41:22 +0530

    modeling api data

--- restapi/app.py
+++ restapi/app.py
@@ -1,71 +1,87 @@
 from flask import Flask , request
+from db import stores,items
+import uuid
+from flask_smorest import abort
 
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "storename": "mystore1",
-        "items": 
-        [
-            {
-                'name': 'chair', 'price': 100
-            },
-            {
-                'name': 'table', 'price': 500
-            }
-        ]
-    },
-    {
-        "storename": "mystore2",
-        "items": 
-        [
-            {
-                'name': 'chair', 'price': 200
-            },
-            {
-                'name': 'table', 'price': 400
-            }
-        ]
-    }
-]
+# stores = [
+#     {
+#         "storename": "mystore1",
+#         "items": 
+#         [
+#             {
+#                 'name': 'chair', 'price': 100
+#             },
+#             {
+#                 'name': 'table', 'price': 500
+#             }
+#         ]
+#     },
+#     {
+#         "storename": "mystore2",
+#         "items": 
+#         [
+#             {
+#                 'name': 'chair', 'price': 200
+#             },
+#             {
+#                 'name': 'table', 'price': 400
+#             }
+#         ]
+#     }
+# ]
+
+# endpoint to all get stores
+@app.get('/store')  
+def get_all_store():
+    return {"stores": list(stores.values())}
 
 
-@app.get('/store')  # endpoint to get store
-def get_store():
-    return {"stores": stores}
+# endpoint to create store with id
+@app.post("/store") 
+def insert_store():
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex # generate unique id 
+    store = {**store_data,"id":store_id}
+    stores[store_id]=store
+    return store, 201 # 201 -: Accepted request
 
 
-@app.post("/store") # endpoint to create store
-def create_store():
-    request_data = request.get_json()
-    new_store = {"storename": request_data["storename"], "items": []}
-    stores.append(new_store)
-    return new_store, 201 # 201 -: Accepted request
+@app.post("/item") # endpoint to create item in store
+def insert_item():
+    item_data = request.get_json()
+    # if item_data["store_id"] not in items or:
+    #     abort(404,message="store not found.")
 
+    if item_data["store_id"] not in stores:
+        return {"message": "store not found."},404
+    
+    item_id = uuid.uuid4().hex
+    item = {**item_data,"id":item_id}
+    items[item_id]=item
+    return {"item": item},201
 
-@app.post("/store/<string:storename>/item") # endpoint to create item in store
-def create_item(storename):
-    request_date = request.get_json()
-    for store in stores:
-        if store["storename"] == storename:
-            new_item = {"name": request_date["name"], "price": request_date["price"]}
-            store["items"].append(new_item)
-            return new_item , 201
-    return "Store not found", 404
+# endpoint to get all items
+@app.get("/item")
+def get_all_item():
+    return {"items": list(items.values())},200
 
-@app.get("/store/<string:storename>") # endpoint to search a store
-def search_store(storename):
-    for store in stores:
-        if store["storename"] == storename:
-            return {"message":"store found."} , 200
-        
-    return {"message":"store not found."}, 404
+# endpoint to search a store by id
+@app.get("/store/<string:store_id>") 
+def get_store_by_id(store_id):
+    try:
+        return stores[store_id],200
+    except KeyError:
+        # return {"message":"store not found."}, 404
+        abort(404,message="store not found.")
 
-@app.get("/store/<string:storename>/item") # endpoint to get all item in a store
-def search_item_in_store(storename):
-    for store in stores:
-        if store["storename"] == storename:
-            return {"items":store["items"]} , 200
-        
-    return {"message":"store not found."}, 404    
+# endpoint to get item by id
+@app.get("/item/<string:item_id>") 
+def get_item_by_id(item_id):
+    try:
+        if items[item_id] in items:
+            return items[item_id]
+    except KeyError:
+        return {"message":"item not found."}, 404    
